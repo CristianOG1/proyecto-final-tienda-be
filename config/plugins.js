aa4bad31d0411622ffb5fa06ec26c12c7639106d
CristianOG1 <23302011@utfv.edu.mx>
2025-11-24 02:40:24 -0600

    feat: cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+export default {
+  'cloudinary-media-library': {
+    enabled: true,
+    config: {
+      cloudName: 'ddlg63caq',
+      apiKey: '692586932574655',
+      encryptionKey: 'I_uymjXc0MqY4O-gPWkVGye9gZEXYZ12',
+    },
+  },
+};
